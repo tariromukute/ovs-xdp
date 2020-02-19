7a995700825b511f652a0627bb45dd7a7535c062
Tariro Mukute <tariromukute@gmail.com>
2020-02-19 05:22:35 +0000

    Removed the error messgaes that were being printed for debug purposes. The problem was that the openvswitch was using the kernel module that came with the linux distro, of which was modifyinng the datapath module hence the new action was failing.

--- datapath/flow_netlink.c
+++ datapath/flow_netlink.c
@@ -3253,7 +3253,6 @@ int ovs_nla_copy_actions(struct net *net, const struct nlattr *attr,
              const struct sw_flow_key *key,
              struct sw_flow_actions **sfa, bool log)
 {
-	OVS_NLERR(log, "Checking ovs_nla_copy_actions");
     int err;
     u32 mpls_label_count = 0;
 
@@ -3261,19 +3260,15 @@ int ovs_nla_copy_actions(struct net *net, const struct nlattr *attr,
     if (IS_ERR(*sfa))
         return PTR_ERR(*sfa);
 
-	OVS_NLERR(log, "Checking ovs_nla_copy_actions eth_p_mpls");
     if (eth_p_mpls(key->eth.type))
         mpls_label_count = hweight_long(key->mpls.num_labels_mask);
 
     (*sfa)->orig_len = nla_len(attr);
     err = __ovs_nla_copy_actions(net, attr, key, sfa, key->eth.type,
                      key->eth.vlan.tci, mpls_label_count, log);
-	OVS_NLERR(log, "Checking ovs_nla_copy_actions __version");
 
     if (err)
         ovs_nla_free_flow_actions(*sfa);
-	
-	OVS_NLERR(log, "Checking ovs_nla_copy_actions getting error %d", err);
 
     return err;
 }
